81794aa1f8a604aa3a58c79d753cc51684cf27b0
Mark Geurts <mark.w.geurts@gmail.com>
2014-12-08 22:04:15 -0600

    Initialized WriteDVH

---
+++ WriteDVH.m
@@ -0,0 +1,4 @@
+function WriteDVH(filename, image, dose)
+% WriteDVH computes the DVH for each structure included in the image input
+% variable given the dose input variable and writes the resulting DVHs to 
+% a comma-separated value file.
